7f3a6cb4d43bb6dcc7d7538fe619f7d68779460c
David Burles <dburles@gmail.com>
2019-06-29 15:00:53 +1000

    Update GlobalStyle.js

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -3,7 +3,8 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   body {
     font-family: ${props => props.theme.fontFamily.text};
-    color: ${props => props.theme.colors.black}
+    color: ${props => props.theme.colors.black};
+    background: ${props => props.theme.colors.grey[1]};
   }
 `;
 
