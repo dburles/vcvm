d2347cd42c9d1eca1e697ed4c912d6678f1da785
David Burles <dburles@gmail.com>
2019-09-30 11:11:37 +1000

    unused

--- src/data.js
+++ src/data.js
@@ -1,15 +1,5 @@
 import data from './data/data.json';
 import alphabeticSort from './lib/alphabetic-sort';
-import identity from './lib/identity';
-import unique from './lib/unique';
-
-export const allTags = unique(
-  data
-    .map(collection => collection.modules.map(module => module.tags).flat())
-    .flat()
-    .map(tag => tag.toLowerCase()),
-  identity,
-).sort(alphabeticSort);
 
 export const allBrands = data
   .map(collection => collection.name)
